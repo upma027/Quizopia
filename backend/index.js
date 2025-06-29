89243115f4f7620664c8863914b86877c131bba3
upma027 <u.pm.a2708@gmail.com>
2025-06-29 21:52:59 +0530

    Update few changes

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
     // }
     
     app.use(cors({
-       origin: "https://quizopia-jade.vercel.app/",
+       origin: "https://quizopia-jade.vercel.app",
        credentials: true,
 }))
 
